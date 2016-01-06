23850d25fa859688d006e2a75f408bfc521603f3
theoderic <dsfreak17@gmail.com>
2016-01-05 20:17:38 -0500

    Added an internal structure to represent a class.

---
+++ UMLGenerator/generator/main/ClassRepresentation.java
@@ -0,0 +1,46 @@
+package generator.main;
+
+public class ClassRepresentation implements IClass, ITraverser {
+	int version;
+	int access;
+	String name;
+	String signature;
+	String superName;
+	String[] interfaces;
+	
+	public ClassRepresentation(int version, int access, String name, String signature, String superName,
+			String[] interfaces) {
+		this.version = version;
+		this.access = access;
+		this.name = name;
+		this.signature = signature;
+		this.superName = superName;
+		this.interfaces = interfaces;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public int getAccess() {
+		return access;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public String getSuperName() {
+		return superName;
+	}
+
+	public String[] getInterfaces() {
+		return interfaces;
+	}
+	
+	
+}
